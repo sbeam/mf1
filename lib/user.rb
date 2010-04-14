be1f2102b5be646fdb78be8ed2e86d252fa4682b
sbeam <sbeam@onsetcorps.net>
2010-04-14 07:08:09 -0400

    using HTTP Basic auth with on-the-fly innocent user record creation

---
+++ lib/user.rb
@@ -0,0 +1,20 @@
+class User
+    
+    # TODO don't use weak DES crypt
+    def self.create(params)
+      id = DB['users'].save({'username' => params[:username], 
+                             'password' => params[:password].crypt((rand*100).to_s), 
+                             'created_at'=>Time.now.to_i})
+      @current_user = DB['users'].find_one(id)
+    end
+
+    # creds = [username, pass]
+    def self.check(creds)
+        @current_user ||= DB['users'].find_one('username' => creds[0])
+        if @current_user.nil?
+            self.create({:username => creds[0], :password => creds[1]})
+        end
+        creds[1].crypt(@current_user['password']) == @current_user['password']
+    end
+
+end
