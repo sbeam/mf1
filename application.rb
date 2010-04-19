94d56de1ce241550d781e0b9c845297a4677e75c
sbeam <sbeam@onsetcorps.net>
2010-04-19 14:22:51 -0400

    hackery to add profile, with working BG image!

--- application.rb
+++ application.rb
@@ -86,7 +86,27 @@ end
 
 get '/profile' do
     protect!
+    @profile = User.new(auth_username)
     haml :profile
+end
+
+post '/profile' do
+    protect!
+    @profile = User.new(auth_username)
+    @profile.set('biography', params[:biog])
+    @profile.set('website', params[:url])
+
+    [:pic, :background].each do |file|
+        if params[file] && (tmpfile = params[file][:tempfile]) && (name = params[file][:filename])
+            @profile.attach_file(file, name, tmpfile)
+        end
+    end
+
+    if @profile.save
+        flash[:notice] = "Your new profile is saved!"
+    end
+
+    redirect '/profile'
 end
 
 get '/reply/:chirp_id' do
@@ -114,29 +134,8 @@ end
 
 helpers do
 
-  # Usage: partial :foo
-  def partial(page, options={})
-    haml page, options.merge!(:layout => false)
-  end
-
-  def time_ago_in_words(timestamp)
-      minutes = (((Time.now.to_i - timestamp).abs)/60).round
-      return nil if minutes < 0
-
-      case minutes
-      when 0 then 'less than a minute ago'
-      when 0..4 then 'less than 5 minutes ago'
-      when 5..49 then minutes.to_s + ' minutes ago'
-      when 50..70 then 'about 1 hour ago'
-      when 70..119 then 'over 1 hour ago'
-      when 120..239 then 'more than 2 hours ago'
-      when 240..1440 then 'about '+(minutes/60).round.to_s+' hours ago'
-      else Time.at(timestamp).strftime('%I:%M %p %d-%b-%Y')
-      end
-  end
-
   def protect!
-    unless authorized?
+    unless authenticated?
       response['WWW-Authenticate'] = %(Basic realm="mf1 userlist")
       throw(:halt, [401, "Not authorized\n"])
     end
@@ -146,10 +145,6 @@ helpers do
       unless session[:did_auth].nil?
           protect!
       end
-  end
-
-  def authorized?
-      authenticated? 
   end
 
   def authenticated?
@@ -171,6 +166,43 @@ helpers do
           @current_user.is_following?(username)
       end
   end
+
+  def get_user_bg(username)
+      unless username.nil?
+          username = username['username'] unless username.is_a? String
+          if User.exists?(username)
+              user = User.new(username)
+              id = user.get('background')
+              unless id.nil?
+                  return "/images/%s" % id.to_s 
+              end
+          end
+      end
+      '/waxwings.jpg'
+  end
+
+
+  # Usage: partial :foo
+  def partial(page, options={})
+    haml page, options.merge!(:layout => false)
+  end
+
+  def time_ago_in_words(timestamp)
+      minutes = (((Time.now.to_i - timestamp).abs)/60).round
+      return nil if minutes < 0
+
+      case minutes
+      when 0 then 'less than a minute ago'
+      when 0..4 then 'less than 5 minutes ago'
+      when 5..49 then minutes.to_s + ' minutes ago'
+      when 50..70 then 'about 1 hour ago'
+      when 70..119 then 'over 1 hour ago'
+      when 120..239 then 'more than 2 hours ago'
+      when 240..1440 then 'about '+(minutes/60).round.to_s+' hours ago'
+      else Time.at(timestamp).strftime('%I:%M %p %d-%b-%Y')
+      end
+  end
+
 
 end
 
--- lib/user.rb
+++ lib/user.rb
@@ -2,8 +2,9 @@
 class User
 
     def initialize(username)
-        @current_user = DB['users'].find_one(:username => username)
+        @user_record = DB['users'].find_one(:username => username)
     end
+
     
     def self.exists?(un)
         DB['users'].find(:username => un).count
@@ -18,24 +19,42 @@ class User
       initialize(params[:username]) 
     end
 
+    def get(field)
+        @user_record[field] if @user_record && @user_record[field]
+    end
+
+    def set(field, value)
+        @user_record[field] = value
+    end
+
+    def save
+        DB['users'].save(@user_record) if @user_record
+    end
+
+    def attach_file(key, filename, tmpfile)
+        @grid = Grid.new(DB)
+        file_id = @grid.put(tmpfile, filename, :safe => true)
+        set(key, file_id)
+    end
+
     # creds = [username, pass]
     def check(creds)
-        if @current_user.nil?
+        if @user_record.nil?
             create({:username => creds[0], :password => creds[1]})
         end
-        creds[1].crypt(@current_user['password']) == @current_user['password']
+        creds[1].crypt(@user_record['password']) == @user_record['password']
     end
 
     def is_following?(username)
-      if !@current_user.nil? && User.exists?(username)
-        if followed_list = @current_user.to_a.assoc('following')
+      if !@user_record.nil? && User.exists?(username)
+        if followed_list = @user_record.to_a.assoc('following')
             followed_list[1].include?(username)
         end
       end
     end
 
     def who_follows
-        @current_user['following']
+        @user_record['following']
     end
 
     def followers(un)
